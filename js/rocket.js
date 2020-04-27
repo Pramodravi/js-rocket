a8b5342ce4833a440946e5aca09c55f5b9b4c181
Pramodravi <61308503+Pramodravi@users.noreply.github.com>
2020-04-27 14:15:14 +0530

    finished !

--- js/rocket.js
+++ js/rocket.js
@@ -13,6 +13,21 @@ var changeState= function (state) {
 			countdownNumber = countdownNumber-1;
 			document.getElementById('countdown').innerHTML = countdownNumber;
 
+			if (countdownNumber > 3 && countdownNumber <= 6){
+				//be nervous
+				document.getElementById('nervous').className = 'nervous show';
+
+			} else {
+				document.getElementById('nervous').className = 'nervous';
+			}
+
+			if (countdownNumber > 1 && countdownNumber <= 4){
+				//cant wait
+				document.getElementById('cant-wait').className = 'cant-wait show';
+			} else {
+				document.getElementById('cant-wait').className = 'cant-wait';
+			}
+
 			if (countdownNumber <= 0) {
 				changeState(3);
 			};
@@ -25,7 +40,7 @@ var changeState= function (state) {
 			console.log('randomNumber:',randomNumber)
 
 			//success
-			if (randomNumber > 5) {
+			if (randomNumber > 2) {
 				changeState(4);
 			} else {
 				changeState(5); // oh no!
